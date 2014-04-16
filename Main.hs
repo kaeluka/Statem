aa39b5fd3e5dd51a955ba006c890ab954571d3d7
Stephan Brandauer <stephan.brandauer@it.uu.se>
2014-04-16 13:24:22 +0200

    simple string matching (DFA) works

--- Main.hs
+++ Main.hs
@@ -1,7 +1,24 @@
 import Statem
+import Data.List
+import Data.Maybe
+import Control.Monad
 
-tr :: Trans Int
-tr _ = True
+matchString :: String -> DFA Int Char
+matchString s =
+  let edges = map (==) s
+      states = map state [1..]
+  in chainFrom (state 0) (zip edges states)
+
+test = connect (state 2) (=='b') (state 0) $ matchString "aa"
+
+
+--ab_star =
+--  (connect (mkState "a") (edge_eq "a") (mkState "b")) `loop` (edge_eq "b")
+
+--runregex :: Statem Char -> String -> Statem Char
+--runregex st s = foldl' apply st s
 
 main = do
+--  mapM_ print (transitions $ fromJust test)
+--  print $ fromJust (test >>= current)
   putStrLn "arst"
--- Statem.hs
+++ Statem.hs
@@ -1,100 +1,78 @@
-module Statem
-       (Statem,
-        Trans,
-        AcceptMode (..),
-        mkState,
-        state_name,
-        apply,
-        connect,
-        tie,
-        statem_main,
-        Edge
-       ) where
+{-# LANGUAGE FlexibleContexts #-}
+
+module Statem (Edge,
+               show,
+               State,
+               Statem (current,transitions),
+               cur_transitions,
+               DFA,
+               NFA,
+               info,
+               state,
+               empty,
+               connect,
+               chainFrom,
+               apply,
+               applyAll) where
 
 import Test.QuickCheck
 import Control.Monad
+import Data.List
 
 ----------- STATEM
 
-type Trans a = (a->Bool)
-newtype Edge a = Edge (Trans a, Statem a)
-data Meta = Meta { name :: String,
-                   accept_mode :: AcceptMode } deriving (Show,Eq)
-data Statem a = SM Meta [Edge a]
+type Edge a = (a->Bool)
+newtype Transition i a = Transition (State i, Edge a, State i)
 
-data AcceptMode = Accepting | Nonaccepting deriving (Show, Eq)
+type DFA i a = Statem Maybe i a
+type NFA i a = Statem [] i a
 
---instance Arbitrary (Int -> Bool) where
---  arbitrary = (arbitrary :: Gen Int) >>= \i -> return (edge_eq i)
---
---arbSt :: Int -> Gen Statem Int
---arbSt 0 = arbitrary :: Gen String >>= \s -> mkState s
---arbSt n = let n_2 = div n 2
---              r = n - n_2
---          in 
---
---instance Arbitrary (Statem Int) where
---  arbitrary = (arbitrary :: Gen Int) >>=
---              \i -> case i of
---                0 -> mk
+instance Show i => Show (Transition i a) where
+  show (Transition (s, _, s')) =
+    (show s) ++ " -?-> " ++ (show s')
 
-meta :: Statem a -> Meta
-meta (SM m _) = m
+data State i = State i deriving (Show,Eq)
 
-edges :: Statem a -> [Edge a]
-edges (SM _ es) = es
+info :: State i -> i
+info (State i) = i
 
-edge_eq :: Eq a => a -> a -> Bool
-edge_eq x = ((==) x)
+data Statem m i a = Statem { transitions :: [Transition i a],
+                             current :: m (State i)}
 
-mkState n am = SM (Meta { name=n, accept_mode=am }) []
+instance (Show i, Show (m (State i))) => Show (Statem m i a) where
+  show st = "Statem { " ++ (show (transitions st)) ++ ", " ++ show (current st) ++ " }"
 
-apply :: MonadPlus m => a -> Statem a -> m (Statem a)
-apply x (SM m edges) =
+--instance Show (m (State i)) => Show (Statem m i a) where
+--  show = "Statem { " ++ show transitions ++ ", " ++ show current 
+
+state i = State i
+
+empty :: MonadPlus m => m (State i) -> Statem m i a
+empty start = Statem { transitions = [], current = start }
+
+chainFrom :: MonadPlus m => State i -> [(Edge a, State i)] -> Statem m i a 
+chainFrom start tss = 
   let
-    valid = map (\(Edge (_,s)) -> s) $ filter (\ (Edge (f,_)) -> f x) $ edges
-    rvs = map return valid
-  in
-   msum rvs
+    (ret, _) =
+      foldl' (\ (acc, lastState) (trans, newState) ->
+               (connect lastState trans newState acc, newState))
+      (empty $ return start, start)
+      tss
+  in ret
 
-state_name :: Statem a -> String
-state_name (SM m _) = name m
+connect :: State i -> Edge a -> State i -> Statem m i a  -> Statem m i a
+connect s t s' sm = sm { transitions = (Transition (s, t, s')):(transitions sm) }
 
--- take two tests and two SMs and make a cycle
-tie :: (Statem a) -> (a -> Bool) -> (a -> Bool) -> Statem a -> Statem a
-tie (SM m1 es1) f1 f2 (SM m2 es2) =
-  let s1' = SM m1 (Edge (f1, s2') : es1)
-      s2' = SM m2 (Edge (f2, s1') : es2)
-  in s1'
+cur_transitions :: (MonadPlus m) => (Eq i) => Statem m i a -> m (Transition i a)
+cur_transitions (Statem {current = cur, transitions = ts}) = do
+  c <- cur
+  msum $ map return $ filter (\(Transition (s,_,_))-> s==c) ts
 
-connect :: (Statem a) -> (Trans a) -> (Statem a) -> (Statem a)
-connect (SM m es) f s2 = SM m (Edge (f,s2):es)
+applyAll :: MonadPlus m => Eq i => [a] -> Statem m i a -> m (Statem m i a)
+applyAll xs st = foldl' (\mst x -> mst >>= \ st -> apply x st) (return st) xs
 
-prop_connect x = let a = mkState "a" Accepting
-                     b = mkState "b" Accepting
-                     conn = connect a (edge_eq x) b
-                 in (apply x conn >>= Just . meta) == Just (meta b)
+apply :: MonadPlus m => Eq i => a -> Statem m i a -> m (Statem m i a)
+apply x st = do
+  (Transition (_,t,s)) <- cur_transitions st
+  if t x then return st { current = return s } else mzero
 
-prop_tie_cycle x y =
-  let sa' = mkState "sa'" Accepting
-      sb' = mkState "sb'" Accepting
-      knot = (tie sa' (edge_eq x) (edge_eq y) sb')
-  in ((apply x knot) >>= (apply y) >>= Just . meta) == Just (meta sa')
-
-prop_loop x =
-  let s = mkState "!!" Accepting
-      s' = tie s (edge_eq x) (edge_eq x) s
-  in (apply x s'
-      >>= (apply x)
-      >>= (apply x)
-      >>= (apply x)
-      >>= (apply x)
-      >>= Just . meta) == Just (meta s)
-
-
-statem_main = do
-  quickCheck (prop_connect :: Int -> Bool)
-  quickCheck (prop_tie_cycle :: Int -> Int -> Bool)
-  quickCheck (prop_loop :: Int -> Bool)
-
-main = statem_main

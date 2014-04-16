ee37a1a538aef9d83b4760e076fc9d82bca871b9
Stephan Brandauer <stephan.brandauer@it.uu.se>
2014-04-16 14:59:33 +0200

    Now with DFA and NFA test

--- Main.hs
+++ Main.hs
@@ -3,22 +3,30 @@ import Data.List
 import Data.Maybe
 import Control.Monad
 
-matchString :: String -> DFA Int Char
+--matchString :: String -> m Int Char
 matchString s =
   let edges = map (==) s
       states = map state [1..]
   in chainFrom (state 0) (zip edges states)
 
-test = connect (state 2) (=='b') (state 0) $ matchString "aa"
+aab_star :: DFA Int Char
+aab_star = connect (state 2) (=='b') (state 0) $ matchString "aa"
 
+--aab_star = aab_star'
 
---ab_star =
---  (connect (mkState "a") (edge_eq "a") (mkState "b")) `loop` (edge_eq "b")
-
---runregex :: Statem Char -> String -> Statem Char
---runregex st s = foldl' apply st s
+aab_or_ab_star :: NFA Int Char
+aab_or_ab_star = connect (state 1) (=='a') (state 0) $ connect (state 2) (=='b') (state 0) $ matchString "aa"
 
 main = do
---  mapM_ print (transitions $ fromJust test)
+  putStrLn "DFA:"
+  print $ applyAll "aabaabaab" (aab_star)
+  putStrLn "NFA (aa):"  
+  print $ applyAll "aa" aab_or_ab_star
+  putStrLn "NFA (aab):"
+  print $ applyAll "aab" aab_or_ab_star
+  putStrLn "NFA (aaaaaab):"
+  print $ applyAll "aaaaaab" aab_or_ab_star
+    
+
+    --  mapM_ print (transitions $ fromJust test)
 --  print $ fromJust (test >>= current)
-  putStrLn "arst"
--- Statem.hs
+++ Statem.hs
@@ -21,11 +21,13 @@ import Data.List
 
 ----------- STATEM
 
-type Edge a = (a->Bool)
+type Edge a = a->Bool
+
 newtype Transition i a = Transition (State i, Edge a, State i)
 
 type DFA i a = Statem Maybe i a
 type NFA i a = Statem [] i a
+
 
 instance Show i => Show (Transition i a) where
   show (Transition (s, _, s')) =
@@ -41,9 +43,6 @@ data Statem m i a = Statem { transitions :: [Transition i a],
 
 instance (Show i, Show (m (State i))) => Show (Statem m i a) where
   show st = "Statem { " ++ (show (transitions st)) ++ ", " ++ show (current st) ++ " }"
-
---instance Show (m (State i)) => Show (Statem m i a) where
---  show = "Statem { " ++ show transitions ++ ", " ++ show current 
 
 state i = State i
 
@@ -61,18 +60,23 @@ chainFrom start tss =
   in ret
 
 connect :: State i -> Edge a -> State i -> Statem m i a  -> Statem m i a
-connect s t s' sm = sm { transitions = (Transition (s, t, s')):(transitions sm) }
+connect s t s' sm = sm { transitions = (Transition (s, t, s')) : (transitions sm) }
 
-cur_transitions :: (MonadPlus m) => (Eq i) => Statem m i a -> m (Transition i a)
+cur_transitions :: (MonadPlus m, Eq i) => Statem m i a -> m (Transition i a)
 cur_transitions (Statem {current = cur, transitions = ts}) = do
   c <- cur
   msum $ map return $ filter (\(Transition (s,_,_))-> s==c) ts
 
-applyAll :: MonadPlus m => Eq i => [a] -> Statem m i a -> m (Statem m i a)
-applyAll xs st = foldl' (\mst x -> mst >>= \ st -> apply x st) (return st) xs
-
-apply :: MonadPlus m => Eq i => a -> Statem m i a -> m (Statem m i a)
-apply x st = do
+new_state :: (MonadPlus m, Eq i) => a -> Statem m i a -> m (State i)
+new_state x st = do
   (Transition (_,t,s)) <- cur_transitions st
-  if t x then return st { current = return s } else mzero
+  guard (t x)
+  return s
+
+applyAll :: (MonadPlus m, Eq i) => [a] -> Statem m i a -> Statem m i a
+applyAll xs st = foldl' (\st x -> apply x st) st xs
+
+apply :: MonadPlus m => Eq i => a -> Statem m i a -> Statem m i a
+apply x st = do
+  st { current = new_state x st }
 
